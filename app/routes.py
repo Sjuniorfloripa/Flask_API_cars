8edbed6e1951b7d3a6fb010cc84856c41ff5c777
Silvano <sjuniorfloripa666@gmail.com>
2025-08-12 16:51:22 -0300

    Adicionadas rotas de buscar por id, atualizar e deletar.

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.models import Carro
 
 
 def init_routes(app):
+
     @app.route('/carros', methods=['GET'])
     def get_carros():
         carros = Carro.query.all()
@@ -20,3 +21,34 @@ def init_routes(app):
         db.session.add(novo_carro)
         db.session.commit()
         return jsonify(novo_carro.to_dict()), 201
+
+    @app.route('/carros/<int:id>', methods=['GET'])
+    def get_carro(id):
+        carro = Carro.query.get(id)
+        if not carro:
+            return jsonify({"erro": "Carro não encontrado"}), 404
+        return jsonify(carro.to_dict()), 200
+
+    @app.route('/carros/<int:id>', methods=['PUT'])
+    def update_carro(id):
+        carro = Carro.query.get(id)
+        if not carro:
+            return jsonify({"erro": "Carro não encontrado"}), 404
+        
+        data = request.json
+        carro.marca = data['marca']
+        carro.modelo = data['modelo']
+        carro.ano = data.get('ano', carro.ano)
+        
+        db.session.commit()
+        return jsonify(carro.to_dict()), 200
+
+    @app.route('/carros/<int:id>', methods=['DELETE'])
+    def delete_carro(id):
+        carro = Carro.query.get(id)
+        if not carro:
+            return jsonify({"erro": "Carro não encontrado"}), 404
+        
+        db.session.delete(carro)
+        db.session.commit()
+        return '', 204
